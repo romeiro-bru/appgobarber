9bce5bbee8880a36a12581ba4774b1bc0e205c3a
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-10-30 14:50:46 -0300

    Create Button and Input

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { RectButtonProperties } from 'react-native-gesture-handler';
+import { Container, ButtonText } from './styles';
+
+interface ButtonProps extends RectButtonProperties {
+    children: string;
+}
+
+const Button: React.FC = ({ children, ...res }) => 
+    <Container {...res}>
+        <ButtonText>{children}</ButtonText>
+    </Container>
+;
+
+export default Button;
