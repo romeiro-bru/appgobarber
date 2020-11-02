ab912f391eac0dc83b2af3e36fef4afc81af1a02
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-11-02 19:54:17 -0300

    Add unform

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { Image, View, ScrollView, KeyboardAvoidingView, Platform, Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -11,6 +14,7 @@ import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 
 
 const SignUp: React.FC = () => {
+    const formRef = useRef<FormHandles>(null);
     const navigation = useNavigation();
 
     return (
@@ -30,11 +34,13 @@ const SignUp: React.FC = () => {
                          <Title>Crie sua conta</Title>
                         </View>
 
-                        <Input name="name" icon="user" placeholder="Nome" />
-                        <Input name="email" icon="mail" placeholder="E-mail" />
-                        <Input name="password" icon="lock" placeholder="Senha" />
+                        <Form ref={formRef} onSubmit={(data) => {console.log(data)}} >                        
+                            <Input name="name" icon="user" placeholder="Nome" />
+                            <Input name="email" icon="mail" placeholder="E-mail" />
+                            <Input name="password" icon="lock" placeholder="Senha" />
+                        </Form>
 
-                        <Button onPress={() => {console.log('adsd')}} >
+                        <Button onPress={() => formRef.current?.submitForm()} >
                             Entrar
                         </Button>                
                     </ Container>
